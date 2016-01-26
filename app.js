c2ec9f0881ed665ff90d0b80ceabf5addaccf2b3
clairelit <clairefromcork@gmail.com>
2016-01-26 14:08:06 +0000

    Listing/Reading the files in the directory/Folder

--- app.js
+++ app.js
@@ -1,4 +1,16 @@
 //Requiring the file system from the node api
 var fs = require("fs");
+console.log("Just about to call the fs.readir");
 
+//Writing the function to read the contents of the SSP-02-001 directory.
+fs.readdir("./", function(err, files){
+	console.log("I got the contents of the directory.");
 
+    //Looping through the files
+	for(var i in files) {
+		console.log("File: " + files[i]);
+	}
+}	
+);
+
+//Now I must read the contents of the files
